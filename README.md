3bb62938e214f38dae98868f7b46d6b404a63d69
Robin Hillyard <r.hillyard@northeastern.edu>
2024-12-08 17:41:32 -0500

    Added sumN method with appropriate tests. Various other minor changes.

--- README.md
+++ README.md
@@ -7,11 +7,17 @@
 - **Your last commit time will be recorded as your submission time**.
 
 ## Problem Statement
-The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+There are two problems to solve.
+First, get F(n): the Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 
 F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
 Given n, calculate F(n).
+
+Second, calculate the sum of the first n Fibonacci numbers using the method you already implemented.
+
+Note that you must handle the "corner" cases correctly.
 
 **Example 1:**
 - **Input** n = 2
--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -1,11 +1,33 @@
 package org.example;
 
 public class Fibonacci {
-//  getN will not be called for n > 92
-    private static long[] memo = new long[93];
 
-    public static long getN(int n) {
-        // TO BE IMPLEMENTED:
-        return -1;
+    /**
+     * Return the <code>n</code>th Fibonacci number.
+     *
+     * @param n the index into the Fibonacci series. <code>n</code> is positive and less than 93.
+     * @return if <code>n</code> < 0 or <code>n</code> > 92 then -1;
+     * else if <code>n</code> < 2 then <code>n</code>;
+     * else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
+     */
+    public long fibN(int n) {
+        return -1L;
     }
+
+    /**
+     * Determine the sum of the first <code>n</code> Fibonacci numbers.
+     *
+     * @param n the index into the Fibonacci series. <code>n</code> is positive and less than 93.
+     * @return the sum of the Fibonacci numbers 0 through n.
+     */
+    public long sumN(int n) {
+        long result = 0;
+        for (int i = 1; i <= n; i++) result += fibN(i);
+        return result;
+    }
+
+    public Fibonacci() {
+    }
+
+    private final long[] memo = new long[93];
 }
