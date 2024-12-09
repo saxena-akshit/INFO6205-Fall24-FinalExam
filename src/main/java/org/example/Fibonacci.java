db29a0b6c2bbe001bc00fb44e222acc7d99c08f4
saxena-akshit <saxena.aksh@northeastern.edu>
2024-12-09 15:08:31 -0500

    feat: add code for fib

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -5,23 +5,46 @@ public class Fibonacci {
     /**
      * Return the <code>n</code>th Fibonacci number.
      *
-     * @param n the index into the Fibonacci series. <code>n</code> is positive and less than 93.
-     * @return if <code>n</code> < 0 or <code>n</code> > 92 then -1;
-     * else if <code>n</code> < 2 then <code>n</code>;
-     * else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
+     * @param n the index into the Fibonacci series. <code>n</code> is positive
+     * and less than 93.
+     * @return if <code>n</code> < 0 or <code>n</code> > 92 then -1; else if
+     * <code>n</code> < 2 then <code>n</code>; else <code>fibN(n-1)</code> +
+     * <code>fibN(n-2)</code>.
      */
     public long fibN(int n) {
-        return -1L;
+        if (n < 0 || n > 92) {
+            return -1L;
+        }
+
+        if (n < 2) {
+            return n;
+        }
+
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+
+        return memo[n];
     }
 
     /**
      * Determine the sum of the first <code>n</code> Fibonacci numbers.
      *
-     * @param n the index into the Fibonacci series. <code>n</code> is positive and less than 93.
+     * @param n the index into the Fibonacci series. <code>n</code> is positive
+     * and less than 93.
      * @return the sum of the Fibonacci numbers 0 through n.
      */
     public long sumN(int n) {
-         return -1L;
+        long res = 0;
+
+        for (int i = 1; i <= n; i++) {
+            res += fibN(i);
+        }
+
+        return res;
     }
 
     public Fibonacci() {
